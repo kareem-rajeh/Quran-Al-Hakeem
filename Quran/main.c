aeb102f988df983524da36a39dda40706a5228b2
kareem <kareem_rajeh@student.uml.edu>
2023-12-14 13:46:20 -0500

    Initial Commit

---
+++ Quran/main.c
@@ -0,0 +1,14 @@
+//
+//  main.c
+//  Quran
+//
+//  Created by Kareem Rajeh on 12/14/23.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
