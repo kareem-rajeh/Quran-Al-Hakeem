726c8807bae96d3cb2373a108e0998b753dc8317
kareem <kareem_rajeh@student.uml.edu>
2023-12-14 14:20:30 -0500

    added a new struct called juzz so I can mantain cleanleness and readability for code base.

--- Quran/main.c
+++ Quran/main.c
@@ -11,18 +11,25 @@
 
 typedef struct surah
 {
+    char* juzz_name;
+    int juzz_number;
     int surah_number;
-    char* name;
+    char* surah_name;
     double num_pages;
     int num_ayahs;
+    char* description; // ex. it is about the day of judgement
 } Surah;
 
+
+typedef struct juzz {
+    Surah* surahs; // size 114
+} Juzz;
 
 typedef struct quran
 {
     int num_juzz;
     int num_of_surahs;
-    Surah* surahs;
+    Juzz* juzz; // size 30
     char* color;
     double juzz_memorized;
 } Quran;
@@ -54,8 +61,8 @@ Surah* init_surah(char* name, double num_pages, int num_ayahs, int surah_number)
 {
     Surah* surah = malloc(sizeof(Surah));
     (*surah).surah_number = surah_number;
-    (*surah).name = malloc(sizeof(char) * str_len(name) + 1);
-    str_cpy((*surah).name, name);
+    (*surah).surah_name = malloc(sizeof(char) * str_len(name) + 1);
+    str_cpy((*surah).surah_name, name);
     (*surah).num_ayahs = num_ayahs;
     (*surah).num_pages = num_pages;
     
@@ -64,14 +71,14 @@ Surah* init_surah(char* name, double num_pages, int num_ayahs, int surah_number)
 }
 
 
-Quran* init_quran(int num_juzz, int num_of_surahs, char* color, double juzz_memorized)
+Quran* init_quran(char* color, double juzz_memorized)
 {
     Quran* quran = malloc(sizeof(Quran));
     
-    (*quran).num_juzz = num_juzz;
-    (*quran).num_of_surahs = num_of_surahs;
-    (*quran).surahs = calloc(sizeof(Surah), num_of_surahs);
-    if ((*quran).surahs == NULL) {
+    (*quran).num_juzz = 30;
+    (*quran).num_of_surahs = 114;
+    (*quran).juzz = calloc(sizeof(Surah), (*quran).num_juzz);
+    if ((*quran).juzz == NULL) {
         exit(1);
     }
     
@@ -91,14 +98,14 @@ void memorize_an_additional_juzz(Quran* quran, double num_of_juzz_memorized) {
 
 void insert_surah(Quran* quran, Surah* surah) {
     int index = (*surah).surah_number;
-    (*quran).surahs[index] = *surah;
+    // (*quran).surahs[index] = *surah;
     
     return;
 }
 
 
 void print_surah(Surah* surah) {
-    printf("Surah (%d) %s has %d ayahs and %.2f pages, ", (*surah).surah_number, (*surah).name, (*surah).num_ayahs, (*surah).num_pages);
+    printf("Surah (%d) %s has %d ayahs and %.2f pages, ", (*surah).surah_number, (*surah).surah_name, (*surah).num_ayahs, (*surah).num_pages);
     
     return;
 }
@@ -106,35 +113,52 @@ void print_surah(Surah* surah) {
 
 void print_quran_at(Quran* quran, int index);
 
-
-void print_quran(Quran* quran)
-{
-    int i = 0;
-    printf("[");
-    while (i < (*quran).num_of_surahs) {
-        if (i == (*quran).num_of_surahs - 1) {
-            if ((*quran).surahs[i].name == NULL) {
-                printf("EMPTY");
-            }
-            else print_surah(&(*quran).surahs[i]);
-            printf("]\n");
-            break;
-        }
-        if ((*quran).surahs[i].name == NULL) {
-            printf("EMPTY, ");
-        }
-        else print_surah(&((*quran).surahs[i]));
-        i++;
-    }
-    
+int num_ayahs_in_surah(Surah* surah) {
+    return (*surah).num_ayahs;
 }
 
 
+double num_pages_in_surah(Surah* surah);
+
+
+int surah_number(Surah* surah)
+{
+    return (*surah).surah_number;
+}
+
+
+
+//void print_quran(Quran* quran)
+//{
+//    int i = 0;
+//    printf("[");
+//    while (i < (*quran).num_of_surahs) {
+//        if (i == (*quran).num_of_surahs - 1) {
+//            if ((*quran).juzz[i].surahs == NULL) {
+//                printf("EMPTY");
+//            }
+//            else print_surah(&(*quran).surahs[i]);
+//            printf("]\n");
+//            break;
+//        }
+//        if ((*quran).surahs[i].name == NULL) {
+//            printf("EMPTY, ");
+//        }
+//        else print_surah(&((*quran).surahs[i]));
+//        i++;
+//    }
+//
+//}
+
+
 int main(int argc, char* argv[]) {
-    Quran* kareems_quran = init_quran(30, 114, "black", 1.5);
+    Quran* kareems_quran = init_quran("black", 1.5);
     Surah* naba = init_surah("Naba", 1.75, 40, 78);
     insert_surah(kareems_quran, naba);
     
-    print_quran(kareems_quran);
+//    print_quran(kareems_quran);
+    
+    printf("number of ayahs in surah Naba is %d\n", num_ayahs_in_surah(naba));
+    
     return 0;
 }
