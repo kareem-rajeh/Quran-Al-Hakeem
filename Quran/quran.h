7e64210e30a311158689a77ffa8a272ed8e7dc8c
kareem <kareem_rajeh@student.uml.edu>
2023-12-14 19:14:24 -0500

    moved code to multi files.

---
+++ Quran/quran.h
@@ -0,0 +1,59 @@
+//
+//  quran.h
+//  Quran
+//
+//  Created by Kareem Rajeh on 12/14/23.
+//
+
+#ifndef quran_h
+#define quran_h
+
+typedef enum boolean {false, true} Bool;
+
+typedef struct surah
+{
+    char* juzz_name;
+    int juzz_number;
+    int surah_number;
+    char* surah_name;
+    double num_pages;
+    int num_ayahs;
+    char* description; // ex. it is about the day of judgement
+} Surah;
+
+
+typedef struct juzz {
+    int number_surahs;
+    Surah* surahs; // size 114
+} Juzz;
+
+typedef struct quran
+{
+    int num_juzz;
+    int num_of_surahs;
+    Juzz* juzz; // size 30
+    char* color;
+    double juzz_memorized;
+} Quran;
+
+
+Quran* init_quran(char* color, double juzz_memorized);
+Quran* init_and_fill_quran(char* color, double juzz_memorized);
+Surah* init_surah(char* juzz_name, int juzz_num, int surah_num, char* surah_name, double num_pages, int num_ayahs, char* description);
+void insert_surah(Quran* quran, Surah* surah);
+void print_surah(Quran* quran, char* surah_name);
+void print_surah_with_description(Quran* quran, char* surah_name);
+void print_surah_description(Quran* quran, char* surah_name);
+void print_entire_juzz(Quran* quran, int juzz_number);
+void __print_surah(Surah surah, Bool last_surah);
+void print_quran(Quran* quran);
+int num_ayahs_in_surah(Surah* surah);
+double num_pages_in_surah(Surah* surah);
+int surah_number(Surah* surah);
+void memorize_an_additional_juzz(Quran* quran, double num_of_juzz_memorized);
+int str_len(char* str);
+void str_cpy(char* dest, char* src);
+Bool str_equal(char* correct_string, char* response);
+void play_game(Quran* quran);
+
+#endif /* quran_h */
